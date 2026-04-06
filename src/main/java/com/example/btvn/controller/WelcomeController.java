3e62da9d2e87537ebab3f57f85a1a7bd431ba089
mtrangcp <haminhtrang545@gmail.com>
2026-04-06 23:08:50 +0700

    ex1

---
+++ src/main/java/com/example/btvn/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.example.btvn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = {"/", "/home"})
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String showWelcome(Model model) {
+        model.addAttribute("staffName", "Nguyễn Văn An");
+        model.addAttribute("department", "Phòng Kỹ thuật");
+
+        return "welcome";
+    }
+}
